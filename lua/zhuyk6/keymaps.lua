bd912c2f5efc6174a9d2c2a9129152e3eb30cea8
zhuyk6 <zhuyk6@foxmail.com>
2022-12-17 18:33:41 +0800

    Fix some bug.

--- lua/zhuyk6/keymaps.lua
+++ lua/zhuyk6/keymaps.lua
@@ -34,16 +34,19 @@ map("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 -- Insert --
 -- Press jk fast to enter normal mode 
 -- map("i", "jk", "<ESC>", opt)
-require("better_escape").setup {
-    mapping = {"jk"}, -- a table with mappings to use
-    timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-    -- example(recommended)
-    -- keys = function()
-    --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-    -- end,
-}
+local ok, better_escape = pcall(require, "better_escape")
+if ok then
+    better_escape.setup {
+        mapping = {"jk"}, -- a table with mappings to use
+        timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+        clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+        keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+        -- example(recommended)
+        -- keys = function()
+        --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+        -- end,
+    }
+end
 
 -- Visual
 -- Stay in indent mode 
